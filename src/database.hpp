9f09010a2b98a868a8ffde61ff5ab0305e1f827a
Sagar Patil <sagarreddypatil@gmail.com>
2024-03-13 01:38:23 -0400

    single header

--- src/database.hpp
+++ src/database.hpp
@@ -1,19 +1,152 @@
 #pragma once
 
-#include "fmapvec.hpp"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include <functional>
 #include <unordered_map>
 
 template<typename T>
+class FileMappedVector {
+    size_t _size;
+    size_t capacity;
+
+    const char* sentinel_magic = "FMAPVEC";
+
+    struct sentinel {
+        char magic[8];
+        size_t used_size;
+    };
+
+    union elem {
+        T data;
+        sentinel sent;
+    };
+
+    int fd;
+    elem* data;
+
+    public:
+    FileMappedVector(std::string loc) {
+        size_t init_size = sizeof(elem) * 32;
+
+        // create file if it doesn't exist
+        fd = open(loc.c_str(), O_RDWR | O_CREAT, 0644);
+
+        if (fd == -1) {
+            std::cerr << "Error: could not open file " << loc << std::endl;
+            return;
+        }
+
+        // get file size
+        struct stat st;
+        fstat(fd, &st);
+
+        if(st.st_size == 0) {
+            // initialize file
+            ftruncate(fd, init_size);
+            st.st_size = init_size;
+        }
+
+        capacity = st.st_size / sizeof(elem);
+
+        if (capacity == 0) {
+            // invalid file
+            std::cerr << "Error: file " << loc << " is invalid" << std::endl;
+            return;
+        }
+
+        // map file to memory
+        data = (elem*)mmap(NULL, capacity * sizeof(elem), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        if (data == MAP_FAILED) {
+            std::cerr << "Error: could not map file " << loc << " to memory" << std::endl;
+            return;
+        }
+
+        // last element is a sentinel, check magic
+        elem* last = &data[capacity - 1];
+        if(strncmp(last->sent.magic, sentinel_magic, 8) != 0) {
+            // initialize sentinel
+            last->sent.used_size = 0;
+            memcpy(last->sent.magic, sentinel_magic, 8);
+        }
+
+        _size = last->sent.used_size;
+    };
+
+    ~FileMappedVector() {
+        msync(data, capacity * sizeof(elem), MS_SYNC);
+        munmap(data, capacity * sizeof(elem));
+        close(fd);
+    };
+
+    void append(const T& new_elem) {
+        if (_size >= capacity - 1) {
+            // resize
+            auto new_capacity = capacity * 2;
+
+            // unmap old memory
+            munmap(data, capacity * sizeof(elem));
+
+            // ftrunc
+            ftruncate(fd, new_capacity * sizeof(elem));
+
+            int flags = MAP_SHARED;
+
+            // remap
+            data = (elem*)mmap(NULL, new_capacity * sizeof(elem), PROT_READ | PROT_WRITE, flags, fd, 0);
+            if (data == MAP_FAILED) {
+                std::cerr << "Error: could not remap file to memory" << std::endl;
+                return;
+            }
+
+            capacity = new_capacity;
+
+            // update sentinel
+            elem* last = &data[capacity - 1];
+            memcpy(last->sent.magic, sentinel_magic, 8);
+            last->sent.used_size = _size;
+        }
+
+        data[_size].data = new_elem;
+        _size++;
+
+        // update sentinel
+        elem* last = &data[capacity - 1];
+        last->sent.used_size = _size;
+    };
+
+    T operator[](size_t i) {
+        if (i >= _size) {
+            std::cerr << "Error: index out of bounds" << std::endl;
+            return T();
+        }
+
+        return data[i].data;
+    };
+
+    size_t size() {
+        return _size;
+    };
+};
+
+template<typename T>
 class Table {
+public:
     struct Entry {
         uint64_t timestamp;
         T value;
     };
 
+private:
     std::shared_ptr<FileMappedVector<Entry>> data;
 
-
 public:
+
     Table(std::string loc) {
         data = std::make_shared<FileMappedVector<Entry>>(loc);
     }
@@ -22,8 +155,8 @@ public:
         data->append({timestamp, value});
     }
 
-    T operator[](size_t idx) {
-        return data->operator[](idx).value;
+    Entry operator[](size_t idx) {
+        return data->operator[](idx);
     }
 
     size_t size() {
@@ -34,10 +167,35 @@ public:
         // tail optimized binary search
         // we know that the data is sorted by timestamp
 
+        if(timestamp <= data->operator[](0).timestamp) {
+            return 0;
+        }
+
+        if(timestamp >= data->operator[](data->size() - 1).timestamp) {
+            return data->size() - 1;
+        }
+
         return _locate(timestamp, 0, data->size() - 1);
     }
 
-    private:
+    std::vector<Entry> reduce(uint64_t t_start, uint64_t t_end, uint64_t dt) {
+        // reduce by dropping data points
+        size_t start = locate(t_start);
+        size_t end = locate(t_end) + 1;
+        end = std::min(end, size());
+
+
+        std::vector<Entry> reduced;
+        for (size_t i = start; i < end; i++) {
+            if (!reduced.size() || reduced.back().timestamp + dt <= data->operator[](i).timestamp) {
+                reduced.push_back(data->operator[](i));
+            }
+        }
+
+        return reduced;
+    }
+
+private:
     size_t _locate(uint64_t timestamp, size_t start, size_t end) {
         if (start == end) {
             return start;
--- src/main.cpp
+++ src/main.cpp
@@ -3,47 +3,5 @@
 #include <vector>
 #include <map>
 
-#include "fmapvec.hpp"
 #include "database.hpp"
 
-
-int bruh() {
-    struct datapoint {
-        uint64_t c;
-        int64_t v;
-    };
-
-    size_t npts = 500000000;
-    Table<datapoint> vec("test.dat");
-
-    // for(uint i = 0; i < (uint)npts; i++) {
-    //     vec.append(i, {i, rand()});
-    // }
-
-    // generate a list of 1000 random numbers between 0 and npts
-    std::vector<size_t> randoms;
-    for(int i = 0; i < 5000000; i++) {
-        randoms.push_back(rand() % npts);
-    }
-
-    volatile size_t index;
-
-    // search for the random indices
-    for(int idx : randoms) {
-        index = vec.locate(idx);
-    }
-
-    return index;
-
-    // sleep(10);
-
-
-    // Database<datapoint> db("db");
-    // db.make_table("test");
-    // db.get_table("test")->append({1, 2});
-}
-
-int main() {
-    bruh();
-    return 0;
-}
